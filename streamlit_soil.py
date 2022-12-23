e4fda5430818e4876c045e4aa25dedf16adea44e
hamza19901990 <74471418+hamza19901990@users.noreply.github.com>
2022-12-24 01:30:19 +0300

    Add files via upload

--- streamlit_soil.py
+++ streamlit_soil.py
@@ -79,7 +79,7 @@ st.write('---')
 # Reads in saved classification model
 import pickle
 load_clf = pickle.load(open('soil_eco.pkl', 'rb'))
-st.header('Prediction of Concrete Compressive Strength (Mpa)')
+st.header('Prediction of UCS (Mpa)')
 
 # Apply model to make predictions
 prediction = load_clf.predict(df)
